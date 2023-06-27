bd1c6cddc7aa91ceba1bccc70914fd09082a566f
504 <504@DESKTOP-BD9B2H9>
2023-06-27 15:29:54 +0900

    20230627

---
+++ team4/public/브랜치/틱택토 _이환희.js
@@ -0,0 +1,11 @@
+function outPutResult(){
+
+	if(resultNumber == 1){
+		alert("승리하였습니다")
+		console.log("승리하였습니다")
+	}else if(resultNumber == 2){
+		alert("패배하였습니다")
+	}else if(resultNumber == 3){
+		alert("무승부입니다")
+	}
+}
