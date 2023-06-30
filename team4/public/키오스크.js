30f31408815ddb28f6f05c4706452a58e1c6bb6b
504 <504@DESKTOP-SI28KUP>
2023-06-30 15:00:25 +0900

    0630

--- team4/public/키오스크.js
+++ team4/public/키오스크.js
@@ -23,6 +23,34 @@ admin // 관리자 계정
  let priceArray = [] //요금리스트
  let giftCardArray = [] //상품권 리스트
  
+console.log('js실행')
+function SignUp(){ console.log('버튼')
+	let Name = document.querySelector('#Name').value; 	// 성명
+	let ID = document.querySelector('#ID').value;		// 아이디	
+	let Pwd = document.querySelector('#Pwd').value; 		// 비밀번호
+	
+	if( ID.length < 8){
+		alert('8자 이상 입력해주세요.')
+	}
+	if( Pwd.length < 8){
+		alert('8자 이상 입력해주세요.')
+	}
+	
+	
+	
+	let account = { Name : Name ,
+					ID : ID ,
+					Pwd : Pwd 
+	}
+	if( Name == '' || ID == '' || Pwd == '' ){
+		alert('회원가입정보를 입력해주세요.')
+		return;
+	}
+	console.log( account );
+	accountArray.push(account);
+ }
+ console.log(accountArray)
+ 
  
  
  
